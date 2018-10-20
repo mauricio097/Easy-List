5b1190e58420a8d9393e086bdf887cd5975bb49f
mauricio097 <junior14_santos@hotmail.com>
2018-10-20 04:59:50 -0300

    Code of page Home Organized

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,70 +1,60 @@
-import React,{Component} from 'react';
-import { View, Text, FlatList, TouchableOpacity} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import Styles from './styles';
 import FontAwesome, { Icons } from 'react-native-fontawesome';
 import Header from '../../components/header';
 
-export default class Home extends Component{
+export default class Home extends Component {
 
   static navigationOptions = {
-    
-      header: null
-      /*title: 'Easy List',
-        headerStyle: {
-          backgroundColor: '#38ADA9',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        }*/
+    header: null
   };
 
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
   state = {
     data: [
-      { id: "00", 
+      {
+        id: "00",
         name: "Lista Savegnago",
-        items:[ 
-                {id: "00",name: "Café",price:"2.19",quantity:"2"},
-                {id: "01",name: "Açucar",price:"3.50",quantity:"5"},
-                {id: "02",name: "Feijão",price:"5.96",quantity:"2"}
-              ] 
+        items: [
+          { id: "00", name: "Café", price: "2.19", quantity: "2" },
+          { id: "01", name: "Açucar", price: "3.50", quantity: "5" },
+          { id: "02", name: "Feijão", price: "5.96", quantity: "2" }
+        ]
       },
-      { id: "01", 
+      {
+        id: "01",
         name: "Lista 2",
-        items:[ 
-                {id: "00",name: "Café",price:"2.19",quantity:"2"},
-                {id: "01",name: "Açucar",price:"3.50",quantity:"5"},
-                {id: "02",name: "Aveia",price:"5.96",quantity:"2"}
-              ] 
+        items: [
+          { id: "00", name: "Café", price: "2.19", quantity: "2" },
+          { id: "01", name: "Açucar", price: "3.50", quantity: "5" },
+          { id: "02", name: "Aveia", price: "5.96", quantity: "2" }
+        ]
       }
     ]
   };
 
-  selectItem(item){
-    this.props.navigation.navigate('Details',item);
+  selectItem(item) {
+    this.props.navigation.navigate('Details', item);
   };
 
-  render(){
-    return(
+  render() {
+    return (
       <View>
-        <Header title='Easy List' 
-          leftComponent={ null} 
-          rightComponent={null}
-          />
-        <View style={Styles.ViewGrid}>
-            <FlatList
+        <Header title='Easy List' />
+        <View style={Styles.containerView}>
+          <FlatList
             data={this.state.data}
             keyExtractor={item => item.id}
             renderItem={({ item }) => {
               return (
-                <TouchableOpacity onPress={ ()=> this.selectItem(item)}>
-                  <View style={Styles.item}>
-                    <Text style={Styles.text}>{item.name}</Text>
-                    <Text style={Styles.IconItem}>
+                <TouchableOpacity onPress={() => this.selectItem(item)}>
+                  <View style={Styles.itemListView}>
+                    <Text style={Styles.itemListText}>{item.name}</Text>
+                    <Text style={Styles.itemListIcon}>
                       <FontAwesome>{Icons.angleRight}</FontAwesome>
                     </Text>
                   </View>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,32 +1,27 @@
 import { StyleSheet } from 'react-native';
 
 const Styles = StyleSheet.create({
-    ViewGrid: {
+    containerView: {
         marginTop: 10,
         marginHorizontal: 10
-    }, 
-    item: {
-      flexBasis: 0,
-      alignItems: "center",
-      backgroundColor: "#FFF",
-      flexGrow: 1,
-      margin: 4,
-      padding: 20,
-      borderRadius: 5,
-      flexDirection: 'row'
     },
-    text: {
-     // color: "#333333",
-      flex:1,
-      fontSize: 20
+    itemListView: {
+        flexBasis: 0,
+        alignItems: "center",
+        backgroundColor: "#FFF",
+        flexGrow: 1,
+        margin: 4,
+        padding: 20,
+        borderRadius: 5,
+        flexDirection: 'row'
     },
-    IconItem: {
+    itemListText: {
+        flex: 1,
+        fontSize: 20
+    },
+    itemListIcon: {
         fontSize: 30
-    },
-    icon: {
-      color: '#FFF',
-      fontSize: 32,
-  },
-  });
+    }
+});
 
 export default Styles;
